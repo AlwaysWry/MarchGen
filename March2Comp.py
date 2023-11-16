95c54b60131b8219c4b1b02194940092895219cc
孙睿 张 <18811693610m0@sina.cn>
2023-11-16 16:56:32 +0800

    add UI text

--- March2Comp.py
+++ March2Comp.py
@@ -79,7 +79,8 @@ def March2Comp(fault_list, fault_model, fp):
 if __name__ == '__main__':
 	march_test_file = 'results/march.m2c'
 	test_logs_file = 'results/testlog'
-	fault_list_file = 'resources/fault_lists/' + 'complete_with_novel'
+	# fault_list_file = 'resources/fault_lists/' + 'complete_with_novel'
+	fault_list_file = sys.argv[1]
 
 	with open("results/generation_report.txt", 'w') as report:
 		report.write("***********************************************************************************\n")
